1141dd221a5cc67f2850696c0353d5b2ea706a64
carlosdiaztl <diaz-alzate@hotmail.com>
2025-06-03 12:34:42 -0500

    updated read me

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Agrega tu URL aquí para el entorno de producción
-  apiUrl: 'https://api-task-list-mongo-7sxj.vercel.app/api/v1', // ¡Cambia esto por tu URL real de producción!
-  apiBase: 'https://api-task-list-mongo-7sxj.vercel.app/' // ¡Cambia esto por tu URL real de producción!
+  apiUrl: 'https://api-task-list-mongo.vercel.app/api/v1', // ¡Cambia esto por tu URL real de producción!
+  apiBase: 'https://api-task-list-mongo.vercel.app/' // ¡Cambia esto por tu URL real de producción!
 };
